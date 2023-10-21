7fd078e4da1db3191d2624717dfb2f48b3054fbf
edmar cassimiro <edmarcassimiro@outlook.com>
2023-10-21 09:41:08 -0300

    alterações

---
+++ script.js
@@ -0,0 +1,14 @@
+function registre() {
+let nome = document.getElementById('nome').value 
+let cpf = document.getElementById('cpf').value
+
+localStorage.setItem('meuNome' , nome.value)
+localStorage.setItem('meuCpf' , cpf.value)
+
+// console.log(`Nome: ${nome}, CPF: ${cpf}`)
+
+// debugger
+console.log(localStorage.getItem('meuNome').valueOf)
+console.log(localStorage.getItem('meucpf').valueOf)
+
+}
